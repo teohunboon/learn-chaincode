a763615b0d382d73ece40e9ee6cd1ac9d220be2b
Teo Hun Boon <teohunboon@gmail.com>
2017-02-01 12:09:51 +0800

    finished the change of test chaincode

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -43,7 +43,11 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 	if len(args) != 1 {
 		return nil, errors.New("Incorrect number of arguments. Expecting 1")
 	}
+	err := stub.PutState("Hello World", []byte(args[0]))
 
+	if err != nil {
+		return nil, err
+	}
 	return nil, nil
 }
 
@@ -54,10 +58,44 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 	// Handle different functions
 	if function == "init" { //initialize the chaincode state, used as reset
 		return t.Init(stub, "init", args)
+	} else if function == "write" { /// reveive write function
+		return t.Write(stub, "write", args)
+	} else if function == "read" { /// invoke read method to retrieve based on the pass in key value
+		return t.Read(stub, "read", args)
 	}
 	fmt.Println("invoke did not find func: " + function) //error
 
 	return nil, errors.New("Received unknown function invocation: " + function)
+}
+
+func (t *SimpleChaincode) Write(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	var key, value string
+	//var err error
+	fmt.Println("Running in Write() function")
+	if len(args) != 2 {
+		return nil, errors.New("Incorrect numbero of arguments ,Expecting 2. name of the key and value to set")
+	}
+	key = args[0]
+	value = args[1]
+	err := stub.PutState(key, []byte(value))
+	if err != nil {
+		return nil, err
+	}
+	return nil, nil
+}
+func (t *SimpleChaincode) Read(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	fmt.Println("Running Read() method!")
+	if len(args) <= 0 {
+		return nil, errors.New("Incorrect number of argument passed in for read operation, 1 argument expected for the key")
+	}
+	value, err := stub.GetState(args[0])
+	if err != nil {
+		fmt.Println("Encountered read error in GetState!")
+		jsonResp := "{\"Error\":\"Failed to get state for " + args[0] + "\"}"
+		return nil, errors.New(jsonResp)
+	}
+	return value, nil
+
 }
 
 // Query is our entry point for queries
@@ -68,6 +106,8 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 	if function == "dummy_query" { //read a variable
 		fmt.Println("hi there " + function) //error
 		return nil, nil
+	} else if function == "read" {
+		return t.Read(stub, "read", args)
 	}
 	fmt.Println("query did not find func: " + function) //error
 
